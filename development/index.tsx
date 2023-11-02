7e0a08ee5efee77d15c4be76193951be15866373
fazzolari17 <42549543+fazzolari17@users.noreply.github.com>
2023-11-02 14:24:14 -0400

    development

--- development/index.tsx
+++ development/index.tsx
@@ -3,12 +3,11 @@ import ReactDOM from 'react-dom/client';
 import { 
   Advanced
 } from '../src';
-import { Main } from '../development/props';
+
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Main />
     <Advanced size={100} displayText={true} textColor='red' textAnimation={true}/>
   </React.StrictMode>
 );
